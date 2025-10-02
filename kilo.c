426f927dc3e69fbbd7a608df195b93671fedac7f
Federico Parisella <darthmorphus@gmail.com>
2025-10-02 15:16:32 +0100

    Enter Raw mode and prints out entered character

--- kilo.c
+++ kilo.c
@@ -1,3 +1,58 @@
+/*** includes ***/
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** data ***/
+
+struct termios orig_termios;
+
+/*** terminal ***/
+
+void die(const char *s) { // Prints an error message and exits program
+  perror(s);
+  exit(1);
+}
+
+void disableRawMode() { // Disables raw mode after exiting
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
+}
+
+void enableRawMode() { // Enters raw mode
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios; // Stores old termios and enters raw mode by disabling flags
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+/*** init ***/
+
 int main() {
+    enableRawMode();
+
+    while (1) { // Reads 1 character at a time
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+        printf("%d ('%c')\r\n", c, c);
+        }
+        if (c == 'q') break; // Quit on 'q'
+    }
+
     return 0;
 }
